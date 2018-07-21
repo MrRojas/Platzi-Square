1b6626de8c4c639dbf84672fdbbc950538981a13
= <=>
2018-07-21 06:40:50 -0400

    seccion contacto

---
+++ src/app/contacto/contacto.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-detalle',
+  templateUrl: './contacto.component.html',
+  styleUrls: ['../app.component.css']
+  
+})
+export class ContactoComponent {
+
+
+  
+}
